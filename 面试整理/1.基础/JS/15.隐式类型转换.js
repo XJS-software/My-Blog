0c8ccc07edd0cb6693ad887c5cc457374f39173a
xujs <907596538@qq.com>
2020-08-08 11:59:01 +0800

    隐式类型转换

--- 面试整理/1.基础/JS/15.隐式类型转换.js
+++ 面试整理/1.基础/JS/15.隐式类型转换.js
@@ -20,6 +20,29 @@ var r6 = [] == true // false => '' == true => 0 == 1
 console.log('r6: ', r6);
 
 
+/** ------ 修改对象 valueOf 方法 ------ */
+var a = [] 
+a.valueOf = function(){
+  return 'array'
+}
+var res1 = a == 'array'
+console.log('res1: ', res1);  // true
 
+var b = {}
+b.valueOf = function(){
+  return 'object'
+}
+var res2 = b == 'object'
+console.log('res2: ', res2); // true
+
+
+/** -------- 为对象新增 Symbol.toPrimitive --------- */
+var o = {}
+o[Symbol.toPrimitive] = function(){
+  console.log('Symbol.toPrimitive is run');
+  return 'object'  
+}
+var res3 = o == 'object'
+console.log('res3: ', res3); // Symbol.toPrimitive is run true
 
 
