889b8fbd3aa4e7a68f1255dca08092709fd12250
xujs <907596538@qq.com>
2020-09-10 21:45:48 +0800

    babel

---
+++ Node/1.Babel/2.test.js
@@ -0,0 +1,12 @@
+import someFun from "./1.test";
+import baz from "./1.test";
+
+/** 经 babel 转义如下 */
+
+("use strict");
+
+var _ = _interopRequireDefault(require("./1.test"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
---
+++ Node/1.Babel/4.test.js
@@ -0,0 +1,12 @@
+export default [1,2,3]
+
+/** 经 babel 转义如下 */
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = [1, 2, 3];
+exports["default"] = _default;
