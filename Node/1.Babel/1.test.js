4aa13e5556d3c64d9f5d9e8037dfe5f16245ec2f
xujinshan <xujinshan1@kingsoft.com>
2020-09-11 08:52:27 +0800

    merge

---
+++ Node/1.Babel/1.test.js
@@ -0,0 +1,30 @@
+// const foo = ()=>{
+//     console.log(this.foo);
+// }
+
+// export function baz (){
+
+// }
+
+// export default function fo(){
+// }
+
+/** 转义如下 */
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.baz = baz;
+exports["default"] = fo;
+
+var _this = void 0;
+
+var foo = function foo() {
+  console.log(_this.foo);
+};
+
+function baz() {}
+
+function fo() {}
